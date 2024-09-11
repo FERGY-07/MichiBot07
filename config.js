d920d92e93b00236f54610a1a8c1a7bf2421f059
FERGY-07 <pnpaaikexd@gmail.com>
2024-09-11 13:39:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,21 +13,20 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["56941827124", 'MichiBot', true], //MichiBot
-["56928561853"], 
-["56928561853"], 
-["56941827124"],
-["56941827124"],
-["56941827124"],
-["56941827124"],
-["56941827124"],
-["56941827124"],
-['56941827124'],
-["56941827124"],
-["56941827124"],
-["56941827124"],
-["56941827124"],
-["56941827124"]]
+["51929457802", 'MclarenBot', true], //Mclaren
+["51929457802"], 
+["51929457802"],
+["51929457802"],
+["51929457802"],
+["51929457802"],
+["51929457802"],
+["51929457802"],
+["51924335279"],
+["51924335279"],
+["51924335279"],
+["51924335279"],
+["51924335279"],
+["51924335279"]]
 
 global.mods = []
 global.prems = []
@@ -125,9 +124,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["593968263524", 'MichiBot', 1], 
-["5492266466080", 'MichiBot', 1],  
-["5521989092076", 'MichiBot', 1]]
+["51929457802", 'MichiBot', 1], 
+["51929457802", 'MichiBot', 1],  
+["51929457802", 'MichiBot', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -135,8 +134,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = `MichiBot` //"MichiBot"
-global.author = `MichiBot` //"MichiBot"
+global.packname = `Mclaren` //"Mclaren"
+global.author = `Mclaren` //"Mclaren"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -170,9 +169,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "MichiBot"
-global.igfg = "MichiBot"
-global.nomorown = "593993684821"
+global.wm = "Mclaren"
+global.igfg = "Mclaren"
+global.nomorown = "51924335279"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
